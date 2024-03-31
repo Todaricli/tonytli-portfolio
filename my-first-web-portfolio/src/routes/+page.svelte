4c859a730fb7a147bd01bd22aae9f9bab6da0faa
Todaricli <leetony347@yahoo.com>
2024-03-31 14:17:40 +1300

    set up tailwindcss

--- my-first-web-portfolio/src/routes/+page.svelte
+++ my-first-web-portfolio/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-center text-3xl font-bold underline text-white">Welcome to SvelteKit</h1>
+<p class="text-center text-white">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.stone.950);
+    }
+</style>
--- my-first-web-portfolio/svelte.config.js
+++ my-first-web-portfolio/svelte.config.js
@@ -1,13 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess: [
+		vitePreprocess({
+			style: {
+				css: {
+					postcss: join(__dirname, 'postcss.config.js')
+				},
+			}
+		})
+	],
 };
-
 export default config;
