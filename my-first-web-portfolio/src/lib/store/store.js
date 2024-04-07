4d5ea819df045505e6d571ec49ec53169f295026
Todaricli <leetony347@yahoo.com>
2024-04-07 13:35:03 +1200

    change global data loading duration just to enable loading animation

--- my-first-web-portfolio/src/lib/store/store.js
+++ my-first-web-portfolio/src/lib/store/store.js
@@ -1,6 +1,6 @@
 import { writable} from "svelte/store";
 
-export const globalDataLoadingDuration = writable(Math.floor(Math.random()* (2000 - 1200 +1) + 1500))
+export const globalDataLoadingDuration = writable(Math.floor(Math.random()* (2000 - 1200 +1) + 1200))
 
 export let mounted = writable(false)
 
