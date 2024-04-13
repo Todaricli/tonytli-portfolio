9c8c2adee8696e7624b041a23d452e082f99d704
Todaricli <leetony347@yahoo.com>
2024-04-13 19:08:45 +1200

    update homepage with actual data

--- my-first-web-portfolio/src/routes/+page.svelte
+++ my-first-web-portfolio/src/routes/+page.svelte
@@ -21,7 +21,7 @@
 	<div class="w-full p-28 flex flex-col justify-center items-start gap-5 pb-20 desktop:pb-24 desktop:pl-44 desktop:pt-32">
 		<div class="flex flex-col justify-start items-start gap-5">
 			<h3 class="w-80 text-lg text-gray-300">
-				Welcome to John Smith's World of Web Wizardry! I'm John, a skilled web developer passionate
+				Welcome to Tony Tuocheng Li's World of Tech Wizardry! I'm Tony, a prospective Software developer passionate
 				about creating stunning online experiences. Explore my portfolio and discover how I can
 				bring your digital dreams to life.
 			</h3>
@@ -35,13 +35,13 @@
 	<div class="text-container flex flex-col items-center justify-end pb-24 overflow-hidden">
 		<div class="whitespace-nowrap text-inner-wrapper overflow-hidden text-white">
 			<div class="text-slides">
-				<h1 class="text-[188px] desktop:text-[220px]">John Smith - Prospective Web Developer</h1>
+				<h1 class="text-[188px] desktop:text-[220px]">Tony T Li - Prospective Software Developer</h1>
 			</div>
 			<div class="text-slides">
-				<h1 class="text-[188px] desktop:text-[220px]">John Smith - Prospective Web Developer</h1>
+				<h1 class="text-[188px] desktop:text-[220px]">Tony T Li - Prospective Software Developer</h1>
 			</div>
 			<div class="text-slides">
-				<h1 class="text-[188px] desktop:text-[220px]">John Smith - Prospective Web Developer</h1>
+				<h1 class="text-[188px] desktop:text-[220px]">Tony T Li - Prospective Software Developer</h1>
 			</div>
 		</div>
 	</div>
--- my-first-web-portfolio/src/routes/projects/projects.js
+++ my-first-web-portfolio/src/routes/projects/projects.js
@@ -1,12 +1,24 @@
 export const projects = [
     {
         slug: "project1",
-        name: "E-commerce Website Development",
-        duration: "6 months",
-        company: "Apple",
-        image: ".././project_icon/project1.png",
-        technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>During my time at Apple, I was part of a team responsible for developing an innovative e-commerce website aimed at enhancing the online shopping experience for Apple customers. Over a period of six months, we utilized a combination of HTML, CSS, and JavaScript to create a sleek and intuitive user interface that seamlessly integrated with Apple's existing ecosystem.</p>
+        name: "Blog Web App",
+        duration: "3 weeks",
+        company: "Course Assignment at UOA",
+        image: ".././project_icon/blog.png",
+        technologies: ["HTML", "CSS", "JavaScript", "Node.js", "Express.js", "SQL", "UUID", "AJAX", "API", "Handlebars", "ER-diagram", "Github"],
+        desc: `<p>Served as the Team Lead, 
+        responsible for delegating workload following Agile methodologies and 
+        utilizing Kanban-style task 
+        allocation, resolving merge conflicts, 
+        and managing workloads. Also, as a 
+        developer, I mainly implemented 
+        features and frameworks such as 
+        comments, a WYSIWYG editor, 
+        database construction, ER-diagrams, 
+        relation schemas, article creation, 
+        article editing, and updating 
+        personal information, as well as 
+        uploading files or images. </p>
         `
     },
     {
@@ -15,80 +27,8 @@ export const projects = [
         duration: "8 months",
         company: "Google",
         image: ".././project_icon/project2.png",
-        technologies: ["HTML", "CSS", "JavaScript", "Vue.js", "Firebase"],
+        technologies: ["HTML", "CSS", "JavaScript", "Svelte", "SvelteKit", "Tailwind CSS"],
         desc: `<p>At Google, I had the opportunity to contribute to the development of a cutting-edge social media platform aimed at connecting users through shared interests and experiences. Over the course of eight months, our team utilized a modern tech stack, including HTML, CSS, JavaScript, Vue.js, and Firebase, to create a feature-rich platform that prioritized user engagement and privacy.</p>
         `
-    },
-    {
-        slug: "project3",
-        name: "Online Learning Platform Development",
-        duration: "1 year",
-        company: "SpaceX",
-        image: ".././project_icon/project3.png",
-        technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>As part of a cross-functional team at SpaceX, I contributed to the development of an online learning platform aimed at providing educational resources and training materials to SpaceX employees. Over the course of one year, we utilized a full-stack tech stack, to create a comprehensive platform that catered to the diverse learning needs of our workforce.</p>`
-    },
-    {
-        slug: "project4",
-        name: "Portfolio Website Development",
-        duration: "1 month",
-        company: "Amazon",
-        image: ".././project_icon/project4.png",
-        technologies: ["HTML", "CSS", "JavaScript", "React"],
-        desc: `<p>During my time at Amazon, I undertook the development of a personal portfolio website to showcase my skills and projects to potential employers and clients. Over the course of one month, I utilized a modern tech stack, including HTML, CSS, JavaScript, and React, to create a sleek and professional website that highlighted my experience and expertise.</p>`
-    },
-    {
-        slug: "project5",
-        name: "Content Management System Development",
-        duration: "9 months",
-        company: "Facebook",
-        image: ".././project_icon/project5.png",
-        technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>At Facebook, I was involved in the development of a CMS aimed at streamlining the creation, publication, and management of content across Facebook's digital properties. Over the course of nine months, our team utilized a full-stack tech stack, to create a robust and scalable platform that met the evolving needs of Facebook's content creators.</p>`
-    },
-    {
-        slug: "project6",
-        name: "Event Management Platform Development",
-        duration: "10 months",
-        company: "Tencent",
-        image: ".././project_icon/project6.png",
-        technologies: ["HTML", "CSS", "JavaScript", "Vue.js", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>During my tenure at Tencent, I contributed to the development of an event management platform aimed at facilitating the planning, organization, and execution of large-scale events and conferences. Over a period of ten months, our team utilized a full-stack tech stack, to create a comprehensive platform that met the needs of event organizers and attendees.</p>`
-    },
-    {
-        slug: "project7",
-        name: "Online Marketplace Development",
-        duration: "1 year",
-        company: "Apple",
-        image: ".././project_icon/project7.png",
-        technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>During my time at Apple, I was involved in the development of an online marketplace aimed at connecting buyers and sellers of digital goods and services. Over the course of one year, our team utilized a full-stack tech stack, including HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB, to create a secure and user-friendly platform that facilitated transactions and fostered community engagement.</p>`
-    },
-    {
-        slug: "project8",
-        name: "Travel Booking Platform Development",
-        duration: "8 months",
-        company: "Google",
-        image: ".././project_icon/project8.png",
-        technologies: ["HTML", "CSS", "JavaScript", "Angular", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>At Google, I contributed to the development of a travel booking platform aimed at simplifying the process of planning and booking trips. Over a period of eight months, our team utilized a full-stack tech stack, including HTML, CSS, JavaScript, Angular, Node.js, Express, and MongoDB, to create a comprehensive platform that catered to the needs of travelers and travel providers.</p>`
-    },
-    {
-        slug: "project9",
-        name: "Fitness Tracking App Development",
-        duration: "6 months",
-        company: "Amazon",
-        image: ".././project_icon/project9.png",
-        technologies: ["HTML", "CSS", "JavaScript", "React Native", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>During my time at Amazon, I contributed to the development of a fitness tracking app aimed at helping users achieve their health and wellness goals. Over a period of six months, our team utilized a full-stack tech stack, to create a mobile application that provided users with personalized workout plans, nutrition tracking, and progress monitoring.</p>`
-    },
-    {
-        slug: "project10",
-        name: "Blog Platform Development",
-        duration: "4 months",
-        company: "Facebook",
-        image: ".././project_icon/project10.png",
-        technologies: ["HTML", "CSS", "JavaScript", "Vue.js", "Node.js", "Express.js", "MongoDB"],
-        desc: `<p>At Facebook, I contributed to the development of a custom blog platform aimed at empowering users to share their thoughts, experiences, and expertise with a global audience. Over a period of four months, our team utilized a full-stack tech stack, including HTML, CSS, JavaScript, Vue.js, Node.js, Express, and MongoDB, to create a platform that prioritized simplicity, customization, and community engagement.</p>`
     }
 ];
